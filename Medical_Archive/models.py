5414f1f9a8c39209edfc66b6821d061978cf6613
Zahra-abbasi <za.abbasi73@gmail.com>
2025-11-04 20:53:27 +0330

    add Medical_Archive

--- Medical_Archive/models.py
+++ Medical_Archive/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MedicalSpecialty(models.Model):
+    specialty_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+
+class History(models.Model):
+    history_id = models.AutoField(primary_key=True)
+    patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
+    history = models.TextField()
+
+    def __str__(self):
+        return f"History {self.history_id} for {self.patient}" 
+    
