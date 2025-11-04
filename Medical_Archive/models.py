8984a1e0ad15711468587827565a0735a03c5eea
Zahra-abbasi <za.abbasi73@gmail.com>
2025-11-04 22:31:20 +0330

    fixin Meta class in Medical_Archive

--- Medical_Archive/models.py
+++ Medical_Archive/models.py
@@ -27,5 +27,5 @@ class History(models.Model):
     class Meta:
         verbose_name = "Patient History"
         verbose_name_plural= "Patient Histories"
-        ordering= ["patient_id"]
+        ordering= ["first_name"]
 
