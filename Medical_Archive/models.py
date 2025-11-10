c2d09f7e4ff55bb527ae4eacec9471fbcd73b2c4
ramin <ramindesu88@gmail.com>
2025-11-10 14:56:47 +0330

    FIX(circular import)

--- Medical_Archive/models.py
+++ Medical_Archive/models.py
@@ -1,32 +1,26 @@
 from django.db import models
-from Accounts.models import Patient
 
-# Create your models here.
 
 class Specialty(models.Model):
     title = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
-    
 
     class Meta:
         verbose_name = "Specialty"
-        verbose_name_plural= "Specialties"
-        ordering= ["title"]
-
+        verbose_name_plural = "Specialties"
+        ordering = ["title"]
 
 
 class History(models.Model):
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    patient = models.ForeignKey('Accounts.Patient', on_delete=models.CASCADE)
     history = models.TextField()
 
     def __str__(self):
-        return f"History for {self.patient.first_name}" 
-    
-  
+        return f"History for {self.patient.first_name}"
+
     class Meta:
         verbose_name = "Patient History"
-        verbose_name_plural= "Patient Histories"
-        ordering= ["patient__first_name"]
-
+        verbose_name_plural = "Patient Histories"
+        ordering = ["patient"]
