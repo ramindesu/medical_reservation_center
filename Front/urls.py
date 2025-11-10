ad8fb0d8f2d2dfbe0e5ed1080597f00284e4c30b
ramin <ramindesu88@gmail.com>
2025-11-10 15:45:43 +0330

    FEAT(contact-us template)

--- Front/urls.py
+++ Front/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import home , about
+from .views import home , about , contact_us
 
 urlpatterns = [
     path('',home , name= 'home'),
-    path('about/' , about, name='about')
+    path('about/' , about, name='about'),
+    path('contact-us/' , contact_us , name= 'contact-us')
 
 ]
--- Front/views.py
+++ Front/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def about(request):
     return render(request , 'about.html')
+
+def contact_us(request):
+    return render(request, 'contact-us.html')
