03bf9f3467cd2cd8d2eba732c800344a62568f2b
atefe farahbod <atefefarahbod@gmail.com>
2025-11-11 16:37:32 +0330

    feat: Add admin panel for Config and Blacklist models

--- Configs/admin.py
+++ Configs/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Config , Blacklist , Doctor , Patient 
 
-# Register your models here.
+@admin.register(Config)
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = ['key', 'value', 'description', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['key', 'value', 'description']
+    ordering = ['key']
+    list_per_page = 20
+    readonly_fields = ['created_at']
+    
+    
+@admin.register(Blacklist)
+class BlacklistAdmin(admin.ModelAdmin):
+    list_display = ['doctor', 'patient', 'reason', 'active','get_doctor_specialty']
+    list_filter = ['active', 'doctor__specialty']
+    search_fields = [
+        'doctor__first_name', 
+        'doctor__last_name',
+        'patient__first_name', 
+        'patient__last_name',
+        'reason'
+    ]
+    list_editable = ['active']
+    ordering = ['-id']
+    
+    def get_doctor_specialty(self, obj):
+        return obj.Doctor.specialty.title
+
