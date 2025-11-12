65b4ef37735c2114a39699349332b4fa3726c20e
Zahra-abbasi <za.abbasi73@gmail.com>
2025-11-12 21:54:59 +0330

    fix and add url

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,8 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Front.urls'))
+    path('',include('Front.urls')),
+    path('', include('medicalarchive.urls')),
 ]
+
+
