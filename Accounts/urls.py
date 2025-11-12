31080ded8b506ebbeb762e87b9422dca3866348f
ramin <ramindesu88@gmail.com>
2025-11-12 21:05:47 +0330

    FIX(urls)

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -7,13 +7,11 @@ urlpatterns = [
     path('doctors/', views.doctors_list, name='doctors_list'),
     path('booking/<int:doctor_id>/', views.booking_page, name='booking_page'),
     
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/templates/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.home_redirect, name='home_redirect'),
 
    
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/accounts/login'), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
  
     path('register/', views.register, name='register'),
