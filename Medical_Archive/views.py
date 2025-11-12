6b0861331275e4ae7190bf31db5a45d04a3f0b27
Zahra-abbasi <za.abbasi73@gmail.com>
2025-11-12 15:52:22 +0330

    create view specialty in Medical_Archive

--- Medical_Archive/views.py
+++ Medical_Archive/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from .models import Specialty
+from .forms import ReservationForm
+from Accounts import Doctor
 
-# Create your views here.
+def specialty_detail_view(request, specialty_id):
+    specialty = get_object_or_404(Specialty, id=specialty_id)
+    doctor = Doctor.objects.filter(specialty=specialty).first()
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            reservation.doctor = doctor
+            reservation.save()
+            return redirect('home')
+    else:
+        form = ReservationForm()
+    return render(request, 'medicalarchive/specialty_detail.html', {
+        'specialty': specialty,
+        'form': form,
+        'doctor': doctor
+    })
