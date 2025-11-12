68c1adeea675c11ef494d6758819f240486fc836
ramin <ramindesu88@gmail.com>
2025-11-12 21:08:02 +0330

    FIX(urls)

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -47,4 +47,5 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='accounts/password_reset_complete.html'
     ), name='password_reset_complete'),
+
 ]
