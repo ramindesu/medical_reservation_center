a0b4fa667c25ab8be5c6de73c6c5495b3645f662
ramin <ramindesu88@gmail.com>
2025-11-12 21:16:24 +0330

    FIX(form)

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -1,16 +1,48 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from Medical_Archive.models import Specialty
+
 
 class UserRegistrationForm(UserCreationForm):
-    ROLE_CHOICES = (
-        ('patient', 'Patient'),
-        ('doctor', 'Doctor'),
+    role = forms.ChoiceField(
+        choices=[
+            (User.Role.PATIENT, "Patient"),
+            (User.Role.DOCTOR, "Doctor"),
+        ],
+        widget=forms.Select(attrs={'class': 'form-select'})
     )
 
-    role = forms.ChoiceField(choices=ROLE_CHOICES, label="Role")
-    specialty = forms.CharField(max_length=100, required=False, label="Specialty (for doctors)")
+    specialty = forms.ModelChoiceField(
+        queryset=Specialty.objects.all(),
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'phone', 'address', 'password1', 'password2', 'role', 'specialty']
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'phone',
+            'address',
+            'password1',
+            'password2',
+            'role',
+            'specialty',
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        role = cleaned_data.get("role")
+        specialty = cleaned_data.get("specialty")
+
+        if role == User.Role.DOCTOR and not specialty:
+            raise forms.ValidationError("Please select a specialty for doctor.")
+        return cleaned_data
+
+
+
+
