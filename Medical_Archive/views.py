8164968938b5e3106a2cf69e437a3fbf7ea98369
ramin <ramindesu88@gmail.com>
2025-11-12 12:36:57 +0330

    ADD(specialties view)

--- Medical_Archive/views.py
+++ Medical_Archive/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Specialty
 # Create your views here.
+def specialties(requst):
+    specialties = Specialty.objects.all()
+    return render(requst , 'Medical_Archive/specialties.html' , {'specialties' : specialties})
