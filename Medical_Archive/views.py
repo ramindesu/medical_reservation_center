f1d662f79632123b5c6d5417e7868eed41fdfa1f
ramin <ramindesu88@gmail.com>
2025-11-12 12:38:44 +0330

    ADD(docs per specialty )

--- Medical_Archive/views.py
+++ Medical_Archive/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Specialty
-# Create your views here.
-def specialties(requst):
+from Accounts.models import Doctor
+
+def specialties(request):
     specialties = Specialty.objects.all()
-    return render(requst , 'Medical_Archive/specialties.html' , {'specialties' : specialties})
+    return render(request, 'Medical_Archive/specialties.html', {'specialties': specialties})
+
+# def doctors_by_specialty(request, specialty_id):
+#     specialty = get_object_or_404(Specialty, id=specialty_id)
+#     doctors = Doctor.objects.filter(specialty=specialty)
+#     return render(request, 'Medical_Archive/doctors_by_specialty.html', {
+#         'specialty': specialty,
+#         'doctors': doctors
+#     })
+
