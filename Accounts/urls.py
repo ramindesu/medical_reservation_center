687b6c371ebcef63083b682b0be75cb67fd5669c
Niloufar-Arab <choubdar.niloufar@gmail.com>
2025-11-12 20:24:34 +0330

    Fix (bugs in doctor register)

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
    
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/accounts/login'), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
  
     path('register/', views.register, name='register'),
