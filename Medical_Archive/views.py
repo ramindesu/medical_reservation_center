c5c6ef6b7d558e852cfd43808a7741257d0cb1e3
ramin <ramindesu88@gmail.com>
2025-11-12 12:40:34 +0330

    ADD(urls)

--- Medical_Archive/views.py
+++ Medical_Archive/views.py
@@ -14,3 +14,4 @@ def specialties(request):
 #         'doctors': doctors
 #     })
 
+
--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Front.urls'))
+    path('',include('Front.urls')),
+    path('' , include('Medical_Archive.urls'))
 ]
