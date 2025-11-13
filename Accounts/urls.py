4539d91fd0ff6d0cddc96fd10e26b8c2a8b6f178
Niloufar-Arab <choubdar.niloufar@gmail.com>
2025-11-13 18:28:41 +0330

    FIX(doctors list template)

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     
     path('doctors/', views.doctors_list, name='doctors_list'),
-    # path('booking/<int:doctor_id>/', views.booking_page, name='booking_page'),
     path('detail/<int:doctor_id>/', views.doctor_details, name='detail_page'),
     path('booking/<int:doctor_id>/', views.doctor_reservation, name='doctor_reservation'),
     
