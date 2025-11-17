e8af3c4951754d2a36ba9caff3a9f278b7cbe9bc
ramin <ramindesu88@gmail.com>
2025-11-17 12:10:08 +0330

    UPDATE(sending request to all doctors)

--- Medical_Archive/views.py
+++ Medical_Archive/views.py
@@ -1,17 +1,56 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Specialty
 from Accounts.models import Doctor
+from Accounts.forms import DoctorReservationForm
+from Reservations.models import Reservations
+from django.contrib import messages
+
 
 def specialties(request):
     specialties = Specialty.objects.all()
-    return render(request, 'specialties.html', {'specialties': specialties})
+    return render(request, "specialties.html", {"specialties": specialties})
 
 def doctors_by_specialty(request, specialty_id):
     specialty = get_object_or_404(Specialty, id=specialty_id)
     doctors = Doctor.objects.filter(specialty=specialty)
-    return render(request, 'doctors_by_specialty.html', {
-        'specialty': specialty,
-        'doctors': doctors
-    })
+
+    if request.method == "POST":
+        form = DoctorReservationForm(request.POST)
+
+        if form.is_valid():
+
+            base_reservation = form.save(commit=False)
+            patient = request.user.patient
 
 
+            for doctor in doctors:
+                Reservations.objects.create(
+                    doctor=doctor,
+                    patient=patient,
+                    date=base_reservation.date,
+                    service=base_reservation.service,
+                    status=Reservations.Status.WAITING
+                )
+
+            messages.success(
+                request,
+                "Your request has been sent to ALL doctors in this specialty. "
+                "You will get an appointment once a doctor accepts."
+            )
+            return redirect("home")
+
+        else:
+            messages.error(request, "Please correct the errors in the form.")
+
+    else:
+        form = DoctorReservationForm()
+
+    return render(
+        request,
+        "doctors_by_specialty.html",
+        {
+            "specialty": specialty,
+            "doctors": doctors,
+            "form": form,
+        },
+    )
