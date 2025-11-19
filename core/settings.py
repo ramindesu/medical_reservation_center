d1a5554b7e155d93e3b328753530d47084c92d39
Hbalforoosh <Hbalforoosh@gmail.com>
2025-11-19 17:23:08 +0330

    fix: commenting lines 101,102 on settings.py

--- core/settings.py
+++ core/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'medical_db'),
         'USER': os.getenv('DB_USER', 'medical_user'),
         'PASSWORD': os.getenv('DB_PASSWORD', '12345'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'Nach(0617)'),
+        # 'USER': os.getenv('DB_USER', 'postgres'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD', 'Nach(0617)'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
