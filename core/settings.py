5ca3ac070599a16cd9672403c97794d0aa4ae706
Zahra-abbasi <za.abbasi73@gmail.com>
2025-11-25 10:54:39 +0330

    edit

--- core/settings.py
+++ core/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'medical_db'),
-        'USER': os.getenv('DB_USER', 'medical_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', '12345'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'zahra137395'),
         # 'USER': os.getenv('DB_USER', 'postgres'),
         # 'PASSWORD': os.getenv('DB_PASSWORD', 'Nach(0617)'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
