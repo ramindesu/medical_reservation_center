74e816c9cfa7608f8903e4454b5dfbe9a9947482
Hbalforoosh <Hbalforoosh@gmail.com>
2025-12-03 22:27:13 +0330

    add: adding new registerfor new model

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User , Doctor, Patient 
+from .models import User, Doctor, Patient, CapacityIncreaseRequest
 from django.contrib.auth.admin import UserAdmin
 from Medical_Archive.models import Specialty
 from Wallet.models import Wallet
@@ -13,22 +13,24 @@ class PatientInline(admin.StackedInline):
     fields = ['monthly_appointment_limit', 'wallet']
     readonly_fields = ['wallet']
 
+
 class DoctorInline(admin.StackedInline):
     model = Doctor
     can_delete = False
     verbose_name_plural = 'Doctor Profile'
     fk_name = 'user'
-    fields = ['medical_code', 'specialty', 'monthly_reservation_capacity', 'avatar', 'wallet']
+    fields = ['medical_code', 'specialty',
+              'monthly_reservation_capacity', 'avatar', 'wallet']
     readonly_fields = ['medical_code', 'wallet']
-    
+
 
 class CustomUserAdmin(UserAdmin):
     inlines = [PatientInline, DoctorInline]
-    list_display = ['username', 'email', 'first_name', 'last_name', 'phone', 'role', 'is_staff', 'is_active', 'get_user_type']
+    list_display = ['username', 'email', 'first_name', 'last_name',
+                    'phone', 'role', 'is_staff', 'is_active', 'get_user_type']
     list_filter = ['role', 'is_staff', 'is_active', 'date_joined']
     search_fields = ['username', 'email', 'first_name', 'last_name', 'phone']
- 
-    
+
     def get_user_type(self, obj):
         if hasattr(obj, 'patient'):
             return 'Patient'
@@ -38,20 +40,24 @@ class CustomUserAdmin(UserAdmin):
             return 'Admin'
         return 'Staff'
     get_user_type.short_description = 'user type'
-    
+
     def get_inline_instances(self, request, obj=None):
         if not obj:
             return []
         return super().get_inline_instances(request, obj)
 
+
 admin.site.register(User, CustomUserAdmin)
 # ----------------------------
 
+
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('get_first_name', 'get_last_name', 'get_email', 'medical_code', 'specialty', 'monthly_reservation_capacity', 'get_active')
+    list_display = ('get_first_name', 'get_last_name', 'get_email', 'medical_code',
+                    'specialty', 'monthly_reservation_capacity', 'get_active')
     list_filter = ('specialty', 'user__active')
-    search_fields = ('user__first_name', 'user__last_name', 'user__email', 'medical_code')
+    search_fields = ('user__first_name', 'user__last_name',
+                     'user__email', 'medical_code')
     fieldsets = (
         (None, {
             'fields': ('user', 'medical_code', 'specialty', 'monthly_reservation_capacity')
@@ -76,12 +82,13 @@ class DoctorAdmin(admin.ModelAdmin):
     get_active.short_description = 'Active'
 
 
-
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ('get_first_name', 'get_last_name', 'get_email', 'get_phone', 'get_active')
+    list_display = ('get_first_name', 'get_last_name',
+                    'get_email', 'get_phone', 'get_active')
     list_filter = ('user__active',)
-    search_fields = ('user__first_name', 'user__last_name', 'user__email', 'user__phone')
+    search_fields = ('user__first_name', 'user__last_name',
+                     'user__email', 'user__phone')
     fieldsets = (
         (None, {
             'fields': ('user',)
@@ -110,6 +117,7 @@ class PatientAdmin(admin.ModelAdmin):
     get_active.short_description = 'Active'
 
 
+admin.site.register(CapacityIncreaseRequest)
 
 # @admin.register(Admin)
 # class AdminAdmin(admin.ModelAdmin):
@@ -142,6 +150,5 @@ class PatientAdmin(admin.ModelAdmin):
 #         return obj.user.active
 #     get_active.boolean = True
 #     get_active.short_description = 'Active'
-    
-# -------------------------
 
+# -------------------------
